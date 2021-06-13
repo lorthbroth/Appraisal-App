93b395e996e6eccf3fa90d721ae602268e417814
lorthbroth <dkgoete@gmail.com>
2021-06-13 15:51:41 +0200

    fixing up the UI

--- App.js
+++ App.js
@@ -4,12 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
 import { decode, encode } from 'base-64';
 
 if (!global.btoa) { global.btoa = encode }
-if (!global.atob) { gloabal.atob = decode }
+if (!global.atob) { global.atob = decode }
 
 const Stack = createStackNavigator();
 
